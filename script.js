64eeb1278d096a1bf1ab2418b6d0a59906520bbe
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-20 17:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
-const element = document.getElementById("level");
-let level = 0;
+function count(ele) {
+	let level = 0;
 
-while (element.parentElement) {
-	element = element.parentElement;
-	level++;
+	while (element.parentElement) {
+		element = element.parentElement;
+		level++;
+	}
+	return level;
 }
+const element = document.getElementById("level");
+let level = count(element);
 
 alert("The level of the element is: " + level);
